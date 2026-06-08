133c72ff5d38df4a6d723d1a1a7580eadc8c0818
abhi-trumpf <Abhishek.Narayanasami@trumpf.com>
2026-06-08 15:49:30 +0530

    Format and cleanup of TwoSumPractise

--- JavascriptPrograms/TwoSumPractise.js
+++ JavascriptPrograms/TwoSumPractise.js
@@ -1,24 +1,16 @@
-
-
-function twosum(arr,target){
-let result = {};  // You are instantiating a new Object (using the Object constructor/literal)
-for (let i=0;i<arr.length;i++){  //everything is done wrt i
+function twosum(arr, target) {
+  let result = {};
+  for (let i = 0; i < arr.length; i++) {
     let number = arr[i];
     let compliment = target - number;
-    if(compliment in result){   //ok it only works because we are only trying to compliment for 
-    // the elements that are already a part of the result map {}.
-        return[result[compliment],i];    //with respect to result and use square brackets.
+    if (compliment in result) {
+      return [result[compliment], i];
+    } else {
+      result[number] = i;
     }
-    else{
-// FIX 2: Store the number as the key, index as the value Key: The Number (e.g., 2) Value: The Index (eg 0)
-        result[number] = i;  // already ulta-bulta
-        // object[val] - index format .
-    }
-}
-return [];
+  }
+  return [];
 }
 
-
-
-let x = twosum([1,2,3,4],3);
+let x = twosum([1, 2, 3, 4], 3);
 console.log(x);
