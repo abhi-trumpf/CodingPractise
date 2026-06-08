508fd4d26e93a955bdb666011b87c9334442af75
abhi-trumpf <Abhishek.Narayanasami@trumpf.com>
2026-06-08 15:28:10 +0530

    format and Cleanup of RemoveDuplicate

--- JavascriptPrograms/RemoveDuplicates.js
+++ JavascriptPrograms/RemoveDuplicates.js
@@ -1,37 +1,27 @@
-
-
-function RemoveDuplivates(arr){
-    let result = {};
-    let correctarray = [];
-    for(let i=0;i<arr.length;i++){
-        let current = arr[i];
-        if(current in result){ // if current is already a part of result object.
-            continue;
-        }
-        else{
-            result[current]= true; // if not , result is always going to be empty.
-            correctarray.push(current);  // we need to push
-        }
+function RemoveDuplivates(arr) {
+  let result = {};
+  let correctarray = [];
+  for (let i = 0; i < arr.length; i++) {
+    let current = arr[i];
+    if (current in result) {
+      continue;
+    } else {
+      result[current] = true;
+      correctarray.push(current);
     }
-    return correctarray;
+  }
+  return correctarray;
 }
 
-
-let a = [1,2,3,4,4,4,5,5,1,6,6];
+let a = [1, 2, 3, 4, 4, 4, 5, 5, 1, 6, 6];
 let x = RemoveDuplivates(a);
 console.log(x);
 
-//below is by using sets 
-
-
-
-//using Sets
-
-function RemoveDuplivates(arr){
-    let set =   new Set(arr);
-    return [...set];   // convert set into an array.
+function RemoveDuplivates(arr) {
+  let set = new Set(arr);
+  return [...set];
 }
 
-let a = [1,2,3,4,4,4,5,5,1,6,6];
+let a = [1, 2, 3, 4, 4, 4, 5, 5, 1, 6, 6];
 let x = RemoveDuplivates(a);
 console.log(x);
