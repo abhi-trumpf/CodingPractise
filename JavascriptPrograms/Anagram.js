2e372ba29610fbf02c0b39bd77fa753e04890b02
abhi-trumpf <Abhishek.Narayanasami@trumpf.com>
2026-06-08 14:35:59 +0530

    making my code more presentable

--- JavascriptPrograms/Anagram.js
+++ JavascriptPrograms/Anagram.js
@@ -1,17 +1,14 @@
+function CheckAnagram(s1, s2) {
+  // just need the variables no need str data type .
+  let CorrectStringone = s1.toLowerCase().split("").sort().join("");
+  let CorrectStringtwo = s2.toLowerCase().split("").sort().join("");
 
-function CheckAnagram(s1,s2){  // just need the variables no need str data type .
-    let CorrectStringone = s1.toLowerCase().split('').sort().join('');
-    let CorrectStringtwo = s2.toLowerCase().split('').sort().join('');
-
-    if(CorrectStringone === CorrectStringtwo){
-        return true;
-    }
-
-    else{
-        return false
-    }
-
+  if (CorrectStringone === CorrectStringtwo) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-let x = CheckAnagram("heart","earth");
+let x = CheckAnagram("heart", "earth");
 console.log(x);
