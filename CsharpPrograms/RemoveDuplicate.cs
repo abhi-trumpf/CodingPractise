b7947ffece06bcc85ce60e4dbbfbe104cdcb873d
abhi-trumpf <Abhishek.Narayanasami@trumpf.com>
2026-06-23 16:03:10 +0530

    added program to remove duplicate

--- CsharpPrograms/RemoveDuplicate.cs
+++ CsharpPrograms/RemoveDuplicate.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 
 namespace CodingPractise.CsharpCoding
 {
-	public class RemoveDuplicate()
-	{
-		public static void removeDuplicate()
-		{
-			String s = "sadnotsad";
-			HashSet<char> NoDuplicate = new HashSet<char>(s);
-			string output = string.Join("", NoDuplicate);
-			Console.WriteLine(output);
-		}
-	}
+    public class RemoveDuplicate()
+    {
+        public static void removeDuplicate()
+        {
+            String s = "sadnotsad";
+            HashSet<char> noDuplicate = new HashSet<char>(s);
+            string output = string.Join("", noDuplicate);
+            Console.WriteLine(output);
+        }
+    }
 }
